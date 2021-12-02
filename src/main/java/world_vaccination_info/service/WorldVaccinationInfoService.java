91ea96e948767e16c0752fa0a867b350e4a8fd61
asunda23 <89808398+asunda23@users.noreply.github.com>
2021-12-02 03:08:50 -0600

    Akash

---
+++ src/main/java/world_vaccination_info/service/WorldVaccinationInfoService.java
@@ -0,0 +1,30 @@
+package world_vaccination_info.service;
+
+
+import world_vaccination_info.dao.WorldVaccinationInfoDao;
+import world_vaccination_info.domain.WorldVaccinationInfo;
+
+/**
+ * logic functions such as register
+ * @author Akash Sunda
+ *
+ */
+public class WorldVaccinationInfoService {
+	private WorldVaccinationInfoDao entity1Dao = new WorldVaccinationInfoDao();
+	
+	/**
+	 * register a Entity1
+	 * @param form
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+	public void create(WorldVaccinationInfo form) throws WorldVaccinationInfoException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+		
+		// check the primary key of Entity1
+		WorldVaccinationInfo entity1 = WorldVaccinationInfoDao.findByCountry(form.getCountry());
+		if(entity1.getCountry()!=null && entity1.getCountry().equals(form.getCountry())) throw new WorldVaccinationInfoException("This country's vaccination rate has been registered!");
+		entity1Dao.add(form);
+	}
+
+}
