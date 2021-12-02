4788c5d47fad8d507438020d7cffacf79c6d50d0
asunda23 <89808398+asunda23@users.noreply.github.com>
2021-12-02 03:07:58 -0600

    Akash

---
+++ src/main/java/world_vaccination_info/service/WorldVaccinationInfoException.java
@@ -0,0 +1,20 @@
+package world_vaccination_info.service;
+
+public class WorldVaccinationInfoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public WorldVaccinationInfoException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public WorldVaccinationInfoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
