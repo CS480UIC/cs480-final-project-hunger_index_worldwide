5bc371ed83af2df53426bddb35b662b8f4eea5b8
rsures3 <71522051+rsures3@users.noreply.github.com>
2021-12-07 21:27:03 -0600

    Update deleteDB.sql

--- sql/deleteDB.sql
+++ sql/deleteDB.sql
@@ -1,2 +1,11 @@
+use hunger_index_worldwide;
 
-drop database hunger_index_worldwide ;
+drop table world_hunger_statistics;
+drop table world_body_fat_percentage;
+drop table world_death_rate;
+drop table world_happiness_index;
+drop table world_income_index;
+drop table world_unemployment_rate;
+drop table world_vaccination_rate;
+drop table user;
+drop table entity1;
