8f59ec621fc1a650afc0686a5fd26441fa76af8b
rsures3 <71522051+rsures3@users.noreply.github.com>
2021-12-07 21:38:10 -0600

    Create queries.sql

---
+++ sql/queries.sql
@@ -0,0 +1,21 @@
+# N Simple Queries
+
+select country, continent
+from world_body_fat_percentage
+where body_fat_percentage > 1.00
+order by country;
+
+select country, continent
+from world_death_percentage
+where death_rate > 2.00
+order by continent;
+
+select country, continent
+from world_happiness_index
+where happiness_index > 3.00
+order by country;
+
+# Complex Queries
+
+
+
