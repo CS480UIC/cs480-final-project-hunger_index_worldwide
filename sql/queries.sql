b628f80f652325fc89e66fa98072ed0ccda8344a
rsures3 <71522051+rsures3@users.noreply.github.com>
2021-12-07 22:16:21 -0600

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -6,7 +6,7 @@ where body_fat_percentage > 1.00
 order by country;
 
 select country, continent
-from world_death_percentage
+from world_death_rate
 where death_rate > 2.00
 order by continent;
 
@@ -15,7 +15,13 @@ from world_happiness_index
 where happiness_index > 3.00
 order by country;
 
-# Complex Queries
+# N Complex Queries
+
+select DTH.death_rate AS death_rates, VC.vaccination_rate AS vaccination_rates, DTH.country
+from world_death_rate DTH 
+INNER JOIN world_vaccination_rate VC
+ON DTH.country = VC.country
+order by DTH.country;
 
 
 
